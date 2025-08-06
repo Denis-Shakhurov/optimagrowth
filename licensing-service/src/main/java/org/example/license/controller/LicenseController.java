f4ba7082113f2a727ba3700fcf719fa443c2d62f
Denis-Shakhurov <disa.sdf86@gmail.com>
2025-08-06 09:54:53 +0700

    #ADD method getLicense for LicenseController

--- licensing-service/src/main/java/org/example/license/controller/LicenseController.java
+++ licensing-service/src/main/java/org/example/license/controller/LicenseController.java
@@ -1,11 +1,27 @@
 package org.example.license.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.license.model.License;
+import org.example.license.service.LicenseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("v1/organization/{organizationId}/license")
 public class LicenseController {
 
+    private final LicenseService licenseService;
 
+    @GetMapping("/{licenseId}")
+    public ResponseEntity<License> getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        License license = licenseService.getLicense(licenseId, organizationId);
+
+        return ResponseEntity.ok(license);
+    }
 }
